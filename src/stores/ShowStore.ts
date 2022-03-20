a86020aedf16f4a181c95766211fc7cda4986261
Mike Moore <mike@fiddlebits.net>
2022-03-20 09:56:59 -0700

    added pinia

---
+++ src/stores/ShowStore.ts
@@ -0,0 +1,33 @@
+import { defineStore, acceptHMRUpdate } from "pinia";
+// import { useLocalStorage } from "@vueuse/core";
+export const useShowStore = defineStore("ShowStore", {
+    state: () => {
+        return {
+
+        };
+    },
+    getters: {
+        // count: (state) => state.items.length,
+        // isEmpty: (state) => state.count === 0,
+
+    },
+    actions: {
+        // addItems(count, item) {
+        //     count = parseInt(count);
+        //     for (let index = 0; index < count; index++) {
+        //         this.items.push({ ...item });
+        //     }
+        // },
+        // clearItem(itemName) {
+        //     this.items = this.items.filter((item) => item.name !== itemName);
+        // },
+        // setItemCount(item, count) {
+        //     this.clearItem(item.name);
+        //     this.addItems(count, item);
+        // },
+    },
+});
+
+if (import.meta.hot) {
+    import.meta.hot.accept(acceptHMRUpdate(useShowStore, import.meta.hot));
+}
