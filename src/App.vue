7f8a9fd3fd87388192babd282474db9ba828361a
Mike Moore <mike@fiddlebits.net>
2022-03-27 10:16:53 -0700

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,105 @@
-<!-- <template>
-  <ion-app style="flex-direction:row; justify-content: unset;">
-    <ion-split-pane content-id="main-content">
-      <ion-menu content-id="main-content" type="overlay">
-        <ion-content>
-          <ion-list id="inbox-list">
-              <ion-item
-              <ion-item
-                @click="selectedIndex = i"
-                router-direction="root"
-                :router-link="p.url"
-                lines="none"
-                detail="false"
-                class="hydrated"
-                :class="{ selected: selectedIndex === i }"
-              >
-                <ion-label>{{ p.title }}</ion-label>
-              </ion-item>
-            </ion-menu-toggle>
-          </ion-list>
-        </ion-content>
-      </ion-menu>
-      <ion-router-outlet id="main-content"></ion-router-outlet>
-    </ion-split-pane>
-  </ion-app>
-</template> -->
+<script setup lang="ts">
+    import {
+        IonApp,
+        IonGrid,
+        IonRow,
+        IonCol,
+        IonContent,
+        IonIcon,
+        IonItem,
+        IonLabel,
+        IonList,
+        IonMenu,
+        IonMenuToggle,
+        IonRouterOutlet,
+        IonSplitPane,
+    } from "@ionic/vue";
+
+    import {
+        archiveOutline,
+        archiveSharp,
+        bookmarkOutline,
+        bookmarkSharp,
+        heartOutline,
+        heartSharp,
+        mailOutline,
+        mailSharp,
+        paperPlaneOutline,
+        paperPlaneSharp,
+        trashOutline,
+        trashSharp,
+        warningOutline,
+        warningSharp,
+    } from "ionicons/icons";
+
+    import { computed, defineComponent, onMounted, provide, ref } from "vue";
+
+    import { useRouter, useRoute } from "vue-router";
+
+    import ShowService from "./services/ShowService";
+
+    import TodoModel from "@/services/TodoModel";
+
+    import Person from "@/services/Person";
+
+    const router = useRouter();
+    const route = useRoute();
+    
+    const selectedIndex = ref(0);
+    const appPages = [
+        {
+            title: "Tabs",
+            url: "/tabs",
+        },
+        {
+            title: "Other",
+            url: "/other",
+        },
+    ];
+
+    const path = window.location.pathname.split("folder/")[1];
+    if (path !== undefined) {
+        selectedIndex.value = appPages.findIndex(
+            (page) => page.title.toLowerCase() === path.toLowerCase()
+        );
+    }
+
+    const screenWidth = ref(screen.width);
+
+    onMounted(() => {
+        window.onresize = () => {
+            screenWidth.value = window.innerWidth;
+            console.log(`screenWidth=${screenWidth.value}`);
+            console.log(`isWideScreen=${isWideScreen.value}`);
+        };
+    });
+
+    const isWideScreen = computed(() => {
+        return screenWidth.value >= 768;
+    });
+
+    const tabsColSize = computed(() => {
+        return isWideScreen.value ? 6.3 : 12;
+    });
+
+    provide("isWideScreen", isWideScreen);
+    provide("screenWidth", screenWidth);
+
+    // const todoModel1: any = TodoModel(["todo 3", "todo 4"]);
+    // console.log(todoModel1.todos);
+    //
+    // const todoModel2: any = TodoModel(["todo 5", "todo 6"]);
+    // console.log(todoModel2.todos);
+    //
+    // const person1 = new (Person as any)("Bob", 38, true);
+    // // creates a Person instance with properties name: Alice, age: 32, isDeveloper: false and a method writesCode
+    // const person2 = new (Person as any)("Alice", 32);
+    //
+    // // prints out: This person does write code
+    // person1.writesCode();
+    // // prints out: this person does not write code
+    // person2.writesCode();
+</script>
 
 <template>
     <ion-app>
@@ -75,110 +151,6 @@
         </ion-content>
     </ion-app>
 </template>
-
-<script setup lang="ts">
-    import {
-        IonApp,
-        IonGrid,
-        IonRow,
-        IonCol,
-        IonContent,
-        IonIcon,
-        IonItem,
-        IonLabel,
-        IonList,
-        IonMenu,
-        IonMenuToggle,
-        IonRouterOutlet,
-        IonSplitPane,
-    } from "@ionic/vue";
-
-    import {
-        archiveOutline,
-        archiveSharp,
-        bookmarkOutline,
-        bookmarkSharp,
-        heartOutline,
-        heartSharp,
-        mailOutline,
-        mailSharp,
-        paperPlaneOutline,
-        paperPlaneSharp,
-        trashOutline,
-        trashSharp,
-        warningOutline,
-        warningSharp,
-    } from "ionicons/icons";
-
-    import { computed, defineComponent, onMounted, provide, ref } from "vue";
-
-    import { useRouter, useRoute } from "vue-router";
-
-    import ShowService from "./services/ShowService";
-
-    import TodoModel from "@/services/TodoModel";
-
-    import Person from "@/services/Person";
-
-    const selectedIndex = ref(0);
-    const appPages = [
-        {
-            title: "Tabs",
-            url: "/tabs",
-        },
-        {
-            title: "Other",
-            url: "/other",
-        },
-    ];
-
-    var todoModel1: any = TodoModel(['todo 3', 'todo 4']);
-    console.log(todoModel1.todos);
-
-    var todoModel2: any = TodoModel(['todo 5', 'todo 6']);
-    console.log(todoModel2.todos);
-
-    var person1 = new (Person as any)("Bob", 38, true);
-    // creates a Person instance with properties name: Alice, age: 32, isDeveloper: false and a method writesCode
-    var person2 = new (Person as any)("Alice", 32);
-
-    // prints out: This person does write code
-    person1.writesCode();
-    // prints out: this person does not write code
-    person2.writesCode();
-    debugger;
-
-    const path = window.location.pathname.split("folder/")[1];
-    if (path !== undefined) {
-        selectedIndex.value = appPages.findIndex(
-            (page) => page.title.toLowerCase() === path.toLowerCase()
-        );
-    }
-
-    let screenWidth = ref(screen.width);
-
-    const router = useRouter();
-    const route = useRoute();
-
-    onMounted(() => {
-        window.onresize = () => {
-            screenWidth.value = window.innerWidth;
-            console.log(`screenWidth=${screenWidth.value}`);
-            console.log(`isWideScreen=${isWideScreen.value}`);
-        };
-    });
-
-    const isWideScreen = computed(() => {
-        return screenWidth.value >= 768;
-    });
-
-    const tabsColSize = computed(() => {
-        return isWideScreen.value ? 6.3 : 12;
-    });
-
-    provide("isWideScreen", isWideScreen);
-    provide("screenWidth", screenWidth);
-</script>
 
 <style>
     .test-class {
